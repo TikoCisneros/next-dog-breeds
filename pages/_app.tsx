dadabb4848c157a53b5aba64150bec6d193f35d7
Luis A. Cisneros G <tiko.cisneros@gmail.com>
2023-01-22 16:50:46 -0500

    feat: apply Nunito_Sans font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,23 @@
 import '../styles/globals.css';
 
+import { Nunito_Sans } from '@next/font/google';
 import { useState } from 'react';
 import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 
 import type { AppProps } from 'next/app';
+
+const nunito = Nunito_Sans({ weight: ['400', '700'], preload: false });
+
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
-
   // Hydrate query cache see: https://dev.to/arianhamdi/react-query-v4-ssr-in-next-js-2ojj
   return (
     <QueryClientProvider client={queryClient}>
       <Hydrate state={pageProps.dehydratedState}>
-        <Component {...pageProps} />
+        <main className={nunito.className}>
+          <Component {...pageProps} />
+        </main>
         <ReactQueryDevtools initialIsOpen={false} />
       </Hydrate>
     </QueryClientProvider>
