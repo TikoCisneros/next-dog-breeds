313e9c5af5a6ddbb1ebcda8a72b4a17a1422d3cd
Luis A. Cisneros G <tiko.cisneros@gmail.com>
2023-01-22 15:57:47 -0500

    build: add react-query and axios

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { useState } from 'react';
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
+import type { AppProps } from 'next/app';
+export default function App({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
+
+  // Hydrate query cache see: https://dev.to/arianhamdi/react-query-v4-ssr-in-next-js-2ojj
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </Hydrate>
+    </QueryClientProvider>
+  );
 }
-
-export default MyApp
