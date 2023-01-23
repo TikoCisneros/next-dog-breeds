696fab34bc8c743589f61643b45c11b5bc4ba4cc
Luis A. Cisneros G <tiko.cisneros@gmail.com>
2023-01-23 16:03:23 -0500

    feat: add SearchBar component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from 'next';
 import DogList from '../components/DogList';
+import SearchBar from '../components/SearchBar';
 
 const Home: NextPage = () => {
   return (
     <>
+      <SearchBar />
       <DogList />
     </>
   );
